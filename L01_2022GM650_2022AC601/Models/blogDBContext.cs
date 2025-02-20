af4126d70e42792632a96366f67378c728aa1516
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-02-20 17:19:43 -0600

    Agregar archivos de proyecto.

---
+++ L01_2022GM650_2022AC601/Models/blogDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace L01_2022GM650_2022AC601.Models
+{
+	public class blogDBContext : DbContext
+	{
+		public blogDBContext(DbContextOptions<blogDBContext> options) : base(options)
+		{
+
+		}
+
+	}
+}
